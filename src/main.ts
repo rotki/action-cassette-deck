34f2293a4fa417c85a3833d4b0970ebb47489f74
Konstantinos Paparas <kelsos86@gmail.com>
2023-03-30 13:52:10 +0200

    feat: adds branch merging support

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,97 @@
 import * as github from '@actions/github';
-import { error, getInput, info, setFailed } from '@actions/core';
+import { debug, error, getInput, info, setFailed } from '@actions/core';
 import { type PullRequest } from '@octokit/webhooks-definitions/schema';
+import { type GitHub } from '@actions/github/lib/utils';
+
+interface MergeStatus {
+  identifier: string;
+  merged: boolean;
+}
+
+interface Repo {
+  owner: string;
+  repo: string;
+}
+
+interface Message extends Repo {
+  number: number;
+  body: string;
+}
+
+type Octokit = InstanceType<typeof GitHub>;
+
+async function createComment(
+  octokit: Octokit,
+  message: Message
+): Promise<void> {
+  const { number, ...repo } = message;
+  await octokit.rest.issues.createComment({
+    ...repo,
+    // eslint-disable-next-line camelcase
+    issue_number: number,
+  });
+}
+
+async function mergePr(
+  octokit: Octokit,
+  { repo, owner }: Repo,
+  prNumber: number
+): Promise<MergeStatus> {
+  const identifier = `${owner}/${repo}#${prNumber}`;
+
+  info(`found ${identifier}, preparing to merge`);
+
+  const mergeResult = await octokit.rest.pulls.merge({
+    owner,
+    repo,
+    // eslint-disable-next-line camelcase
+    pull_number: prNumber,
+  });
+
+  const merged = mergeResult.data.merged;
+  return { identifier, merged };
+}
+
+async function mergeBranch(
+  octokit: Octokit,
+  { repo, owner }: Repo,
+  base: string,
+  branch: string
+): Promise<MergeStatus> {
+  const identifier = `${owner}/${repo}/tree/${branch}`;
+
+  info(`found ${identifier}, preparing to merge`);
+
+  await octokit.rest.repos.merge({
+    owner,
+    repo,
+    base,
+    head: branch,
+  });
+
+  const ref = `heads/${branch}`;
+
+  info(`branch ${ref} was merged successfully, preparing to delete`);
+
+  await octokit.rest.git.deleteRef({
+    owner,
+    repo,
+    ref,
+  });
+
+  return { identifier, merged: true };
+}
 
 async function run(): Promise<void> {
   try {
     const githubToken: string = getInput('token', { required: true });
     const repo: string = getInput('cassette_repo', { required: true });
+    const defaultBranch: string = getInput('default_branch') ?? 'develop';
 
     const octokit = github.getOctokit(githubToken);
     const { context } = github;
+    const mainRepo = context.repo;
+    const cassetteRepo = { ...mainRepo, repo };
 
     if (!context.payload.pull_request) {
       info(`Didn't detect a PR`);
@@ -16,60 +99,75 @@ async function run(): Promise<void> {
     }
 
     const pr = context.payload.pull_request as PullRequest;
+
     if (!pr.merged) {
       info(`PR #${pr.number} was not merged`);
       return;
     }
 
-    const headRef = pr.head.ref;
-    const { owner, repo: originalRepo } = context.repo;
+    const branch = pr.head.ref;
 
     const matchingPrs = await octokit.rest.pulls.list({
-      owner,
-      repo,
+      ...cassetteRepo,
       state: 'open',
-      head: `${owner}:${headRef}`,
+      head: `${mainRepo.owner}:${branch}`,
     });
 
-    if (!matchingPrs || matchingPrs.data.length === 0) {
-      info(`No open matching PRs found for ${headRef}`);
+    let branchFound = false;
+    try {
+      const targetBranch = await octokit.rest.repos.getBranch({
+        ...cassetteRepo,
+        branch,
+      });
+
+      if (targetBranch.data.protected) {
+        setFailed(`branch ${branch} is protected, bailing`);
+        return;
+      }
+
+      branchFound = true;
+    } catch (e) {
+      if (e instanceof Error) {
+        debug(e.message);
+      }
+    }
+
+    let identifier = '';
+    let merged = false;
+
+    if (matchingPrs && matchingPrs.data.length > 0) {
+      ({ identifier, merged } = await mergePr(
+        octokit,
+        cassetteRepo,
+        matchingPrs.data[0].number
+      ));
+    } else if (branchFound) {
+      ({ identifier, merged } = await mergeBranch(
+        octokit,
+        cassetteRepo,
+        defaultBranch,
+        branch
+      ));
+    } else {
+      info(`No open matching PRs/branches found for ${branch}`);
       return;
     }
 
-    const cassettePr = matchingPrs.data[0];
-    const prLink = `${owner}/${repo}#${cassettePr.number}`;
+    const message: Message = {
+      ...mainRepo,
+      number: pr.number,
+      body: merged
+        ? `${identifier} was successfully merged`
+        : `Could not merge ${identifier} automatically`,
+    };
 
-    info(`found ${prLink} with head ref: ${headRef} preparing to merge`);
+    await createComment(octokit, message);
 
-    const mergeResult = await octokit.rest.pulls.merge({
-      owner,
-      repo,
-      // eslint-disable-next-line camelcase
-      pull_number: cassettePr.number,
-    });
-
-    if (mergeResult.data.merged) {
-      const body = `${prLink} was successfully merged`;
-      info(body);
-      await octokit.rest.issues.createComment({
-        owner,
-        repo: originalRepo,
-        // eslint-disable-next-line camelcase
-        issue_number: pr.number,
-        body,
-      });
+    if (merged) {
+      info(message.body);
     } else {
-      const body = `Could not merge ${owner}/${repo}#${cassettePr.number} automatically`;
-      setFailed(body);
-      error(body);
-      await octokit.rest.issues.createComment({
-        owner,
-        repo: originalRepo,
-        // eslint-disable-next-line camelcase
-        issue_number: pr.number,
-        body,
-      });
-      return;
+      error(message.body);
+      setFailed(message.body);
     }
   } catch (e) {
     if (e instanceof Error) {
